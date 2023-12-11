29ff2f08cde179079327bdf2bbe3403edec1b4a9
Omkar Rajendra Lokhande <77824136+Omkar4141@users.noreply.github.com>
2023-12-11 18:44:39 +0530

    Add files via upload

--- CoffeeMakerTest.java
+++ CoffeeMakerTest.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) 2009,  Sarah Heckman, Laurie Williams, Dright Ho
- * All Rights Reserved.
- * 
- * Permission has been explicitly granted to the University of Minnesota 
- * Software Engineering Center to use and distribute this source for 
- * educational purposes, including delivering online education through
- * Coursera or other entities.  
- * 
- * No warranty is given regarding this software, including warranties as
- * to the correctness or completeness of this software, including 
- * fitness for purpose.
- * 
- * Modifications
- * 20171113 - Michael W. Whalen - Extended with additional recipe.
- * 20171114 - Ian J. De Silva   - Updated to JUnit 4; fixed variable names.
- */
 package edu.ncsu.csc326.coffeemaker;
 
 import static org.junit.Assert.assertTrue;
@@ -28,39 +11,14 @@ import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 
 public class CoffeeMakerTest {
 	
-	//-----------------------------------------------------------------------
-	//	DATA MEMBERS
-	//-----------------------------------------------------------------------
-	private Recipe recipe1;
+		private Recipe recipe1;
 	private Recipe recipe2;
 	private Recipe recipe3;
 	private Recipe recipe4;
 	private Recipe recipe5;
 	
 	private Recipe [] stubRecipies; 
-	
-	/**
-	 * The coffee maker -- our object under test.
-	 */
-	private CoffeeMaker coffeeMaker;
-	
-	/**
-	 * The stubbed recipe book.
-	 */
-	private RecipeBook recipeBookStub;
-	
-	
-	//-----------------------------------------------------------------------
-	//	Set-up / Tear-down
-	//-----------------------------------------------------------------------
-	/**
-	 * Initializes some recipes to test with, creates the {@link CoffeeMaker} 
-	 * object we wish to test, and stubs the {@link RecipeBook}. 
-	 * 
-	 * @throws RecipeException  if there was an error parsing the ingredient 
-	 * 		amount when setting up the recipe.
-	 */
-	@Before
+		@Before
 	public void setUp() throws RecipeException {
 		
 		recipeBookStub = mock(RecipeBook.class);
